6b9251ebbe3427d817ce04cbcdff5c6ffeaa6aab
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-01 12:54:43 +0530

    Update player.php

--- player.php
+++ player.php
@@ -1,1 +1,18 @@
-<video src="<?php echo($_GET['s']); echo($_GET['ep']); ?>.mp4" controls autoplay></video>
+<title>PHOTOCALL.TV - Streaming</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="http://www.espai.jazztel.es/favicon2.ico" type="image/x-icon" />
+<body style="margin:0">
+<script src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
+<div id="player"></div>
+<script>var player = new Clappr.Player({
+source: "<?php echo $_GET['vid'] ?>",
+plugins: [LevelSelector, ChromecastPlugin],
+parentId: "#player",
+height: "100%",
+width: "100%",
+autoPlay: true,
+});
+</script>
+</body>
