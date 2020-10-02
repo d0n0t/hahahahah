abdee9fedd5110bb01b628f2e8a82a30cf74cbf4
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-02 21:46:13 +0530

    Update player.php

--- player.php
+++ player.php
@@ -1,36 +1,18 @@
 <title>brr</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <body style="margin:0">
-  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
-  <script type="text/javascript" src="//cdn.jsdelivr.net/gh/ewwink/clappr-quality-selector-plugin@latest/quality-selector.js"></script>
+  <<script src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
 <script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
 <div id="player"></div>
-<script>
-  
-  var player = new Clappr.Player({
-  source: "<?php echo ['vid'] ?>",
-  plugins: [QualitySelector],
-  qualitySelectorConfig: {
-    title: 'Quality',
-    labels: {
-      2: 'High', // 500kbps
-      1: 'Med', // 240kbps
-      0: 'Low', // 120kbps
-    },
-    defaultQuality: 0, // start with quality 0 or Lowest
-    labelCallback: function(playbackLevel, customLabel) {
-      return customLabel + playbackLevel.level.height + 'p'; // High 720p
-    }
-  },
-  events: {
-    onPlay: function() {
-      setTimeout(function() {
-        var levels = player.getPlaybackQuality();
-        console.log(levels); // log the levels
-        player.setPlaybackQuality(2); // Change to highest level
-      }, 10000); // fired after 10 second playing
-    }
-  }
+<script>var player = new Clappr.Player({
+source: "<?php echo ['vid'] ?>",
+plugins: [LevelSelector, ChromecastPlugin],
+parentId: "#player",
+height: "100%",
+width: "100%",
+autoPlay: true,
 });
-  </script>
+</script>
 </body>
+</html>
