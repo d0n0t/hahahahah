7b118ddc62a2b0fce453a9028517583f44b34afa
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-02 21:49:12 +0530

    Update player.php

--- player.php
+++ player.php
@@ -1,19 +1,37 @@
-<title>brr</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<body style="margin:0">
-  <<script src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
-<script src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
-<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
-<div id="player"></div>
-<script>
-  var player = new Clappr.Player({
-source: "<?php echo ['vid'] ?>",
-plugins: [LevelSelector, ChromecastPlugin],
-parentId: "#player",
-height: "100%",
-width: "100%",
-autoPlay: true,
-});
-</script>
+<html>
+<head>
+  <title>Clappr Player</title>
+  <meta charset="UTF-8">
+  <!-- Player -->
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/clappr/0.2.86/clappr.min.js"></script>
+  <!-- Video resolution switcher -->
+  <script type="text/javascript" src="//cdn.jsdelivr.net/gh/clappr/clappr-level-selector-plugin@latest/dist/level-selector.min.js"></script>
+</head>
+
+<body style="margin:0"> 
+  <div id="player"></div>
+  <script>
+    var player = new Clappr.Player({
+      source: "<?php echo ['vid'] ?>",
+      parentId: "#player",
+      plugins: {
+        core: [LevelSelector]
+      },
+      levelSelectorConfig: {
+        //Optional : custom labels
+        title: '動画品質',
+        labels: {
+          0: 'Very low',
+          1: 'Low ',
+          2: 'Medium ',
+          3: 'HQ',
+          4: 'High',
+          5: 'Very high',
+          6: 'Extremy high',
+          7: 'Highest'
+        },
+      },
+    });
+  </script>
 </body>
 </html>
