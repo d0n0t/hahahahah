203ac5eacff11fe0140217c164decdcf445e0924
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-02 22:07:09 +0530

    Update player.php

--- player.php
+++ player.php
@@ -1,18 +1,41 @@
-<title>PHOTOCALL.TV - Streaming</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="http://www.espai.jazztel.es/favicon2.ico" type="image/x-icon" />
-<body style="margin:0">
-<script src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
-<script src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
-<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
-<div id="player"></div>
-<script>var player = new Clappr.Player({
-source: "<?php echo $_GET['vid'] ?>",
-plugins: [LevelSelector, ChromecastPlugin],
-parentId: "#player",
-height: "100%",
-width: "100%",
-autoPlay: true,
-});
-</script>
+<head>
+    <style>*{margin:0;padding:0;}</style>
+    <script data-cfasync="false" src="http://jwpsrv.com/library/SakQCEfSEeOHhRIxOQfUww.js"></script>
+</head>
+<body>
+   <div id="playerjxJRqLMFuJaA_wrapper" style="position: relative; display: block; width: 100%; height: 490px;">
+      <a id="beforeswfanchor0" href="#playerjxJRqLMFuJaA" tabindex="0" title="Flash start" style="border:0;clip:rect(0 0 0 0);display:block;height:1px;margin:-1px;outline:none;overflow:hidden;padding:0;position:absolute;width:1px;" data-related-swf="playerjxJRqLMFuJaA"></a>
+
+      <object type="application/x-shockwave-flash" data="http://p.jwpcdn.com/6/12/jwplayer.flash.swf" width="100%" height="100%" bgcolor="#000000" id="playerjxJRqLMFuJaA" name="playerjxJRqLMFuJaA" class="jwswf swfPrev-beforeswfanchor0 swfNext-afterswfanchor0" tabindex="0">
+         <param name="allowfullscreen" value="true">
+         <param name="allowscriptaccess" value="always">
+         <param name="seamlesstabbing" value="true">
+         <param name="wmode" value="opaque">
+       </object>
+
+       <a id="afterswfanchor0" href="#playerjxJRqLMFuJaA" tabindex="0" title="Flash end" style="border:0;clip:rect(0 0 0 0);display:block;height:1px;margin:-1px;outline:none;overflow:hidden;padding:0;position:absolute;width:1px;" data-related-swf="playerjxJRqLMFuJaA"></a>
+
+       <div id="playerjxJRqLMFuJaA_aspect" style="display: none;">
+       </div>
+
+       <div id="playerjxJRqLMFuJaA_jwpsrv" style="position: absolute; top: 0px; z-index: 10;"><div class="afs_ads" style="width: 1px; height: 1px; position: absolute; background: transparent;">&nbsp;
+       </div>
+    </div>
+
+  <script data-cfasync="false" type="text/javascript">
+    jwplayer('playerjxJRqLMFuJaA').setup({
+    file: '<?php echo $_GET['vid'] ?>',
+        aboutlink: 'http://skysports.com',
+        controls: 'true',
+        title: 'Football Live Streamings.Com',
+        width: '100%',
+        height: '490',
+        stretching: 'uniform',
+        autostart: 'true',
+        primary: 'flash',
+        skin: 'vapor',
+        androidhls: 'true',     
+    });
+  </script>
+
 </body>
