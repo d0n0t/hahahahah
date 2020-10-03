e91f90fdfcd6691035ba2dea4874a5f44b91e7bc
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-03 22:43:06 +0530

    Update updater.sh

--- init/updater.sh
+++ init/updater.sh
@@ -1,5 +1,4 @@
 $ git config credential.helper store
 $ git push https://github.com/d0n0t/hahahahah.git
-
 Username for 'https://github.com': <USERNAME>
 Password for 'https://USERNAME@github.com': <PASSWORD>
