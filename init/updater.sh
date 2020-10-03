9409950daa7a76d3873231fe76b12096fa346b94
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-03 22:24:59 +0530

    Update updater.sh

--- init/updater.sh
+++ init/updater.sh
@@ -1,39 +1,5 @@
-repo="hahahahah"
-rm -rf ${repo}
+$ git config credential.helper store
+$ git push https://github.com/owner/repo.git
 
-echo "You're running the USERGE-X Updater script"
-echo " "
-# Get username of user
-echo -n "Enter your GitHub username: "
-read userName
-echo " "
-
-# Check if user has forked or has the same repo name
-echo -n "Have you forked USERGE-X from code-rgb or has the same repo name i.e. USERGE-X? [y/n]: "
-read fork
-echo " "
-
-if [[ "${fork}" == 'y' ]]; then
-	git clone https://github.com/${userName}/${repo}.git
-	cd ${repo}
-elif [[ "${fork}" == 'n' ]]; then
-	echo -n "Enter your repo name: "
-	read repo
-	git clone https://github.com/${userName}/${repo}.git
-	cd ${repo}
-else
-        echo " "
-        echo "You were only supposed to enter y or n"
-        exit 1
-fi
-
-echo " "
-echo "Updating your USERGE-X"
-git pull https://github.com/d0n0t/hahahahah.git
-git diff
-git commit -m "😎"
-git push
-echo " "
-echo "Updated"
-cd ..
-rm -rf ${repo}
+Username for 'https://github.com': <USERNAME>
+Password for 'https://USERNAME@github.com':<PASSWORD>
