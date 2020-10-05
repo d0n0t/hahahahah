e5ad16f0e5656eca1758cf7e21f8410c517b48f0
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-05 09:37:16 +0530

    Create onetime.sh

---
+++ init/onetime.sh
@@ -0,0 +1,8 @@
+echo -n "Enter your GitHub username: "
+read email
+git config --global user.name "${email}"
+
+echo -n "Enter your GitHub username: "
+read username
+git config --global user.name "${username}"
+
