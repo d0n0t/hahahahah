fa1abeab9a01e2910e01672e0ff314190202b821
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-05 09:42:18 +0530

    Update updater.sh

--- init/updater.sh
+++ init/updater.sh
@@ -7,16 +7,21 @@ echo " "
 echo -n "Enter your GitHub repo: "
 read repo
 echo " "
+# Get passwd of user
+echo -n "Enter your GitHub password: "
+read passwd
+echo " "
+
 #selection
 repo="${repo}"
 
 rm -rf ${repo}
-git clone https://github.com/${userName}/${repo}.git
+git clone git clone http://${userName}:${passwd}@github.com/${userName}/${repo}.git
 	cd ${repo}
 #updater
 echo " "
 echo "Updating your USERGE-X"
-git pull https://github.com/d0n0t/hahahahah.git
+git pull http://${userName}:${passwd}@github.com/${userName}/${repo}.git
 git diff
 git commit -m "."
 git push
