de7be7e87f91c72e820866133491b6119f883e29
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-08 16:00:53 +0530

    Update init.sh

--- init/init.sh
+++ init/init.sh
@@ -1,1 +1,10 @@
-. init/updater.sh
+echo -n "Github Username: "
+read userName
+echo " "
+git config --global user.name "${userName}"
+
+echo -n "Github Email : "
+read eMail
+echo " "
+git config --global user.email ${eMail}
+
