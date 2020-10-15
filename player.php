0d8ece543c34f4ea1f6eaeb21562ac66bfd6f08b
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-15 09:51:19 +0530

    Create player.php

---
+++ player.php
@@ -0,0 +1,19 @@
+<title>PHOTOCALL.TV - Streaming</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="http://www.espai.jazztel.es/favicon2.ico" type="image/x-icon" />
+<body style="margin:0">
+<script src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js"></script>
+<div id="player"></div>
+<script>var player = new Clappr.Player({
+source: "<?php echo $_GET['vid'] ?>",
+mimeType: "application/x-mpegURL",
+plugins: [LevelSelector, ChromecastPlugin],
+parentId: "#player",
+height: "100%",
+width: "100%",
+autoPlay: true,
+});
+</script>
+</body>
