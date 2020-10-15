85e7663859dc1930528625751b3e0002e6b77117
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-15 09:39:09 +0530

    Delete onetime.sh

--- init/onetime.sh
+++
@@ -1,8 +0,0 @@
-echo -n "Enter your GitHub username: "
-read email
-git config --global user.name "${email}"
-
-echo -n "Enter your GitHub username: "
-read username
-git config --global user.name "${username}"
-
